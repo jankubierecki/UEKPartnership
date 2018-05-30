b92d26bb99fb583709d396a622c1f391be8d5f69
jankubierecki <jankubierecki@gmail.com>
2018-05-30 17:29:29 +0000

    Changes deployment settings

--- UEKpartnerships/production_settings.py
+++ UEKpartnerships/production_settings.py
@@ -1,8 +1,13 @@
 from .settings import *
 import os
-ALLOWED_HOSTS = ['kubierecki.pl', 'www.kubierecki.pl', '46.101.53.236', 'web']
+ALLOWED_HOSTS = ['kubierecki.pl', 'www.kubierecki.pl']
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 DATABASES = {
     'default': {
