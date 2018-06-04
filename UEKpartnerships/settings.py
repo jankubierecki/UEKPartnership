01540400705b0ffe761a5ddfe5107d830f5266c3
Jan Kubierecki <jankubierecki@gmail.com>
2018-06-04 16:03:37 +0200

    added custom 400/401/403/404/500 error sites;

--- UEKpartnerships/settings.py
+++ UEKpartnerships/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'UEKpartnerships.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'university/templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
