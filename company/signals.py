f2ce07d702092191094031445227ffbcc453936f
Jan Kubierecki <jankubierecki@gmail.com>
2018-06-15 08:43:25 -0400

    modified signals;

--- company/signals.py
+++ company/signals.py
@@ -8,7 +8,6 @@ import logging
 from django.utils import timezone
 from company import models
 
-
 logger = logging.getLogger(__name__)
 
 company_email_changed = django.dispatch.Signal(providing_args=["company"])
@@ -19,26 +18,20 @@ def email_notification_time(email, time):
     models.EmailInformedUsers.objects.create(email=email, created_at=time)
 
 
-def is_email_notified(email):
-    return models.EmailInformedUsers.objects.filter(email=email).exists()
-
-
 @receiver(company_email_changed)
 def send_mail_to_company(sender, **kwargs):
     now = timezone.now()
     company = kwargs["company"]
-    if send_email(company.email, now):
-        company.privacy_email_date_send = now
-        company.save(update_fields=["privacy_email_date_send"])
+    company.privacy_email_date_send = send_email(company.email, now)
+    company.save(update_fields=["privacy_email_date_send"])
 
 
 @receiver(company_contact_person_email_changed)
 def send_mail_to_company_contact_person(sender, **kwargs):
     now = timezone.now()
     company_contact_person = kwargs["company_contact_person"]
-    if send_email(company_contact_person.email, now):
-        company_contact_person.privacy_email_date_send = now
-        company_contact_person.save(update_fields=["privacy_email_date_send"])
+    company_contact_person.privacy_email_date_send = send_email(company_contact_person.email, now)
+    company_contact_person.save(update_fields=["privacy_email_date_send"])
 
 
 def get_recipient(email):
@@ -51,10 +44,12 @@ def send_email(email, now):
     message = render_to_string("message_to_company.txt", {'email': email})
     with transaction.atomic():
         models.EmailInformedUsers.objects.select_for_update().all()
-        if not is_email_notified(email):
+        email_notified_list = models.EmailInformedUsers.objects.filter(email=email)
+        if not email_notified_list:
             mail(subject="Informacja o przetwarzaniu panstwa danych", message=message,
                  from_email=settings.EMAIL_HOST_USER,
                  recipient_list=[recipient])
             email_notification_time(email, now)
-            return True
-    return False
+            return now
+        else:
+            return email_notified_list[0].created_at
