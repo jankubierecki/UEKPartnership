69b9f2aa27ff837d2ccfa83294727928953e289a
Jan Kubierecki <jankubierecki@gmail.com>
2018-06-26 13:41:46 -0400

    nginx certbot;

--- runserver.sh
+++ runserver.sh
@@ -10,5 +10,5 @@ done
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-gunicorn UEKpartnerships.wsgi --log-file /var/log/gunicorn/error_log --access-logformat /var/log/gunicorn/access_log --workers=${NUMBER_OF_CORES} -b 0.0.0.0:8000
+gunicorn UEKpartnerships.wsgi --log-file /var/log/gunicorn/log --access-logformat /var/log/gunicorn/access_log --workers=${NUMBER_OF_CORES} -b 0.0.0.0:8000
 
