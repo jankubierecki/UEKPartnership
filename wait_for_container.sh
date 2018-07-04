1592fd0b2e9652d35bebdc64b2159d12e514d661
Jan Kubierecki <jankubierecki@gmail.com>
2018-07-04 14:30:32 -0400

    changed production settings;

--- wait_for_container.sh
+++ wait_for_container.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
-if [ "`docker-compose ps -q ${1}`" -eq "" ]; then
-    echo "PRAWDA"
-else
-    echo "NIE PRAWDA"
-
-fi
-
-#while [ "`docker-compose ps -q ${1}`"=="" ]; do
-#   echo "Waiting for ${1}"
-#  sleep 0.1;
-#done;
+while [ "`docker-compose ps -q ${1} | wc -l`" -lt 1 ]; do
+   echo "Waiting for ${1}"
+  sleep 0.1;
+done;
 
 echo "Container ${1} is ready"
