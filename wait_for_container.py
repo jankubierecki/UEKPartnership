6f9ca69975776b22f9ed1e3ade74b5a8d90d336c
Jan Kubierecki <jankubierecki@gmail.com>
2018-07-25 13:04:09 -0400

    wait_for_container.py

---
+++ wait_for_container.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import io
+from time import sleep
+
+from compose.cli.main import TopLevelCommand, project_from_options
+from contextlib import redirect_stdout
+
+
+def get_container_id(container_name):
+    options = {
+        "--quiet": True,
+        "--filter": "status=running",
+        "nginx": True,
+        "--services": False,
+        "SERVICE": [container_name],
+    }
+
+    project = project_from_options(os.path.dirname(os.path.abspath(__file__)), options)
+    cmd = TopLevelCommand(project)
+    result = io.StringIO()
+
+    with redirect_stdout(result):
+        cmd.ps(options)
+
+
+
+    return result.getvalue()
+
+
+def main():
+    print("waiting for containers...", sys.argv[1:])
+
+    for container_name in sys.argv[1:]:
+        while get_container_id(container_name) == "":
+            print("waiting for: ", container_name)
+            sleep(1)
+
+    print("all containers are ready")
+
+
+if __name__ == "__main__":
+    main()
--- wait_for_container.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-while [ "`docker-compose ps -q ${1} | wc -l`" -lt 1 ]; do
-   echo "Waiting for ${1}"
-  sleep 0.1;
-done;
-
-echo "Container ${1} is ready"
