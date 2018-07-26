ec10c51b2d150b4a74af392a91677e954f3057b3
Jan Kubierecki <jankubierecki@gmail.com>
2018-07-26 11:13:23 -0400

    waiting for postgres in production;

--- wait_for_postgres.sh
+++ wait_for_postgres.sh
@@ -1,3 +1,3 @@
 until docker run --rm --link uekpartnership_postgres_1:pg --net docker-compose_default postgres:alpine pg_isready -U postgres -h pg; do sleep 1; done
 
-until docker run --rm --link [CONTAINER]:pg --net [NETWORK] postgres:9.5 pg_isready -U postgres -h pg; do sleep 1; done
+
