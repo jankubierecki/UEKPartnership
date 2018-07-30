2998089b403764993b1f9de535e4e2548d238ea4
jankubierecki <jankubierecki@gmail.com>
2018-07-30 23:23:39 +0200

    enables dot characters in university emails

--- university/validators.py
+++ university/validators.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 def email_validation(email):
-    reg = '^[_a-z0-9-]+@uek.krakow.pl$'
+    reg = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@uek.krakow.pl$'
     if not re.match(reg, email):
         raise ValidationError(
             _('Email musi kończyć się na @uek.krakow.pl'),
