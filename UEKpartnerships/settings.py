f92ae27a1064dded7073f63a690616abe83f4b04
jankubierecki <jankubierecki@gmail.com>
2018-09-01 23:30:01 +0200

    test

--- UEKpartnerships/settings.py
+++ UEKpartnerships/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'partnerships',
         'USER': 'postgres',
-        'PASSWORD': 'online13',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '5433',
     }
 }
 
