03b10eca43886dbef8416b2d79dd0f7175f69912
jankubierecki <jankubierecki@gmail.com>
2018-09-05 17:52:35 +0200

    tests coverage;

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 coverage run --source="." ./manage.py test --no-input
-coverage report --omit="UEKpartnerships/*","manage.py" --skip-covered --fail-under=50
+coverage report --omit="UEKpartnerships/*","manage.py","wait_for_container.py","venv/*"  --skip-covered --fail-under=50
