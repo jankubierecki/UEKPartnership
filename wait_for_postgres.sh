5738c2e18eb740e75b080723232fd24d8cfa966a
jankubierecki <jankubierecki@gmail.com>
2018-11-05 19:48:49 +0100

    fixes wait_for_postgres.sh

--- wait_for_postgres.sh
+++ wait_for_postgres.sh
@@ -1,3 +1,8 @@
-until docker run --rm --link uekpartnership_postgres_1:pg --net uekpartnership_default postgres:10-alpine pg_isready -U postgres -h pg; do sleep 1; done
-echo "postgres is ready"
+docker run -d --rm --link uekpartnership_postgres_1:pg --net uekpartnership_default postgres:10-alpine
 
+while ! pg_isready
+do
+    echo "waiting for start"
+    sleep 10
+done
+echo "postgres is ready"
