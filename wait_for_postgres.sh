34e1ac4a46301a9a844dad177288f2e155583d8a
jankubierecki <jankubierecki@gmail.com>
2018-11-13 14:21:44 +0100

    fixes postgre no response;

--- wait_for_postgres.sh
+++ wait_for_postgres.sh
@@ -1,3 +1,1 @@
-until docker run --rm --link uekpartnership_postgres_1:pg --net uekpartnership_default postgres:10-alpine pg_isready -U postgres -h pg; do sleep 1; done
-echo "postgres is ready"
-
+until docker run --rm --link uekpartnership_postgres_1 :pg --net docker-compose_default postgres:alpine pg_isready -U postgres -h pg; do sleep 1; done
