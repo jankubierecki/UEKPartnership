8c8d4634114771b9b96b7e3366a61f415b3df33a
jankubierecki <jankubierecki@gmail.com>
2019-03-02 09:30:24 +0100

    adds sentry

--- UEKpartnerships/settings.py
+++ UEKpartnerships/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
